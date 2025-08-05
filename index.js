8c8d01b814c53c8acd27785544d0b1d042be8409
Linus Ngwee <linuslincom@gmail.com>
2025-08-05 12:25:25 +0100

    broadcasting

--- index.js
+++ index.js
@@ -20,7 +20,8 @@ io.on('connection', (socket) => {
   console.log('A user connected');
 
   socket.on('chat message', (msg) => {
-    console.log('Message received: ' + msg);
+    // console.log('Message received: ' + msg);
+    io.emit('chat message', msg);
   })
 
   socket.on('disconnect', () => {
